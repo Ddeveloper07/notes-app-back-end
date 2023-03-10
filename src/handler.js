12b946ac84ae143a36db771adbba552c30c42bbb
Ddeveloper <diksaanandaa@gmail.com>
2023-03-10 10:20:48 +0800

    initial commit

--- src/handler.js
+++ src/handler.js
@@ -84,7 +84,7 @@ const editNoteByIdHandler = (request, h) => {
 
     const response = h.response({
       status: 'success',
-      message: 'Catatan berhasil ditambahkan',
+      message: 'Catatan berhasil diperbarui',
     });
 
     response.code(200);
